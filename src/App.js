78cfdc961c36f5ffc4d8e2597e5d9add3fa21458
hiepmarin <tranlochiep1111@gmail.com>
2021-07-22 21:39:47 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My information</h1>
+        <br/>
+        <h2>Scroll down to see my information</h2>
       </header>
+      <body className="App-body">
+        <h3>Facebook</h3>
+      </body>
     </div>
   );
 }
